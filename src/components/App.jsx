d11617c0b2945b674aca1ee7b121b6cef577b4c5
Pahal Jain <pahajain225@gmail.com>
2024-04-15 19:26:29 +0530

    added local storage functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const restoredNotes = JSON.parse(localStorage.getItem("notes"));
   const [notes, setNotes] = useState([]);
 
+  useEffect(() => {
+    if (notes) {
+      setNotes(restoredNotes);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
   function addNote(newNote) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return [...prevNotes, newNote];
     });
   }
 
   function deleteNote(id) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
@@ -40,4 +51,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
